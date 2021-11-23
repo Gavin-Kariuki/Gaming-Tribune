863acfa99598727d28ab02787450b62bdaa1f29e
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-11-23 19:15:04 +0300

    test manenos

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+    # set up method
+    def setUp(self):
+        self.gavin = Editor(first_name = 'Esa', last_name = 'Gavin', email = 'gavinkariuki@gmail.com')
+    
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.gavin,Editor))
