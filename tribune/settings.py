5ff06120bf57e4f5a9e9c08cb1b9fbc7ac96edd7
Gavin-Kariuki <gavin.kariuki@student.moringaschool.com>
2021-11-23 17:29:36 +0300

    fuck

--- tribune/settings.py
+++ tribune/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tribune',
-        'USER': 'esa',
+        'USER': 'esagavin',
         'PASSWORD': 'moringa',
     }
 }
