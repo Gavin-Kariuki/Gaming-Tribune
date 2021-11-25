a671b8a94aec7ba6d77f5a57881a9bb56d0604aa
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-11-25 17:21:05 +0300

    add

--- tribune/settings.py
+++ tribune/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-MODE= os.environ.get("MODE")
+#MODE= os.environ.get("MODE",default="production")
 SECRET_KEY = os.environ.get('SECRET_KEY')
 #DEBUG = config('DEBUG', default=False, cast=bool)
 DEBUG = os.environ.get('DEBUG',False)
